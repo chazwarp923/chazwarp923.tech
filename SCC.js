9d6220c786fcaaee1e77cdd3ea1485eea867b9b3
Chaz <chazwarp923@users.noreply.github.com>
2022-01-26 16:38:50 -0500

    Update SCC.js

--- SCC.js
+++ SCC.js
@@ -1,10 +1,11 @@
 function onLoad() {
 	var stowRateValue = null;
 	var stowFrame = document.getElementById("stowFrame");
-	var stowWIPRow = document.getElementById("stowWIPRow");
+	var stowWIPMin = document.getElementById("stowWIPMin");
+	var stowWIPMax = document.getElementById("stowWIPMax");
 	if(stowFrame.contentDocument != null) {
 		stowRateValue = stowFrame.contentDocument.getElementById("metricValue").textContent;
-		stowWIPRow[1].innerHTML = "Upper Limit: " + stowRateValue / 2;
-		stowWIPRow[2].innerHTML = "Lower Limit: " + stowRateValue / 4; 
+		stowWIPMin.innerHTML = "Upper Limit: " + stowRateValue / 2;
+		stowWIPMax.innerHTML = "Lower Limit: " + stowRateValue / 4; 
 	}
 }
