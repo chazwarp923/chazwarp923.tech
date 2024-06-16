fdf5ab33ef96f77badb3a1493b22d433e74a7e9e
Chaz <chazwarp923@users.noreply.github.com>
2024-06-15 21:19:46 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,5 @@
-<?php
-echo $_SERVER['HTTP_HOME'];
-?>
+<html lang="en-US">
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<?php echo $_SERVER['HTTP_HOME']; ?>
+</body>
+</html>
